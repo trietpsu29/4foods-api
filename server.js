f9a41ccb90e1c8b6f31441000974b436404d7e2c
trietpsu29 <112594221+trietpsu29@users.noreply.github.com>
2025-10-06 15:08:32 +0700

    chore: init express server with MongoDB and dotenv

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("API running..."));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
