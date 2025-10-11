3f083c2e402431d3d305277740b7418cf9f580a1
trietpsu29 <112594221+trietpsu29@users.noreply.github.com>
2025-10-11 17:59:38 +0700

    chore: change Order.js, Product.js, User.js to CommonJS, add .dockerignore, .env.example

--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema(
   {
@@ -13,4 +13,4 @@ const productSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export default mongoose.model("Product", productSchema);
+module.exports = mongoose.model("Product", productSchema);
--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
@@ -17,4 +17,4 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export default mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
