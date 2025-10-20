ba6a41fd8be09bb58b003d1d2c1a673ca9337442
trietpsu29 <112594221+trietpsu29@users.noreply.github.com>
2025-10-20 22:05:41 +0700

    feat: ensure email is required in user schema

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
   {
     name: String,
-    email: { type: String, unique: true },
+    email: { type: String, required: true, unique: true },
     phone: String,
     password: String,
     avatar: String,
